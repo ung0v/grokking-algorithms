b7a70775b76c3226d5a2e44323e8145c724cf47e
vuongvq <vuong.pw@gmail>
2025-03-26 14:37:59 +0700

    *

---
+++ 05-hash-table/index.ts
@@ -0,0 +1,91 @@
+class HashTable {
+  table: any[];
+  size: number;
+
+  constructor() {
+    this.table = new Array(127);
+    this.size = 0;
+  }
+
+  _hash(key: string) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  set(key: string, value: string | number) {
+    const index = this._hash(key);
+
+    // Prevent collision logic handle
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size++;
+  }
+
+  get(key: string) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          return this.table[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  remove(key: string) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
+  display() {
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(
+        ([key, value]) => `[ ${key}: ${value} ]`
+      );
+      console.log(`${index}: ${chainedValues}`);
+    });
+  }
+}
+
+const ht = new HashTable();
+
+ht.set("France", 111);
+
+// "Spain" and "ǻ" both return the same hash value because the number 507 is the sum of both of their ASCII code
+ht.set("Spain", 150);
+ht.set("ǻ", 192);
+
+ht.display();
+// 83: [ France: 111 ]
+// 126: [ Spain: 150 ],[ ǻ: 192 ]
+console.log("Hash table size", ht.size); // 3
+
+ht.remove("Spain");
+ht.display();
+// 83: [ France: 111 ]
+// 126: [ ǻ: 192 ]
